257d840d025d340ebfbf1ac72cbcb379f8e46d8d
rahman700 <mkmahfuzurrahman700@gmail.com>
2025-06-24 04:48:30 +0600

    variable

---
+++ variable.py
@@ -0,0 +1,8 @@
+name = "Mahfuzur Rahman"
+age = 25
+cgpa = 3.50
+
+print("My name is : "+ name)
+print("My age : "+ str(age))
+print("My CGPA : "+ str(cgpa))
+print(name + "has scored",cgpa,"in the secondary exam")
